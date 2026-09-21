c8993e7fea15490f755f4e07d8ace05a05dee570
shreyayadav30 <150824225+shreyayadav30@users.noreply.github.com>
2026-09-21 23:17:09 +0530

    Configure backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -52,8 +52,8 @@ app.get("/api/books/:id", (req, res) => {
   res.json(book);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
